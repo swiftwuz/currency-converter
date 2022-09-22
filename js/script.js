4b5491492ee2f6d3fdc2c408ff2f8fcf1ca7e25e
Chantelle Osafo <you@example.com>
2022-09-22 13:01:01 +0000

    working backend

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const dropList = document.querySelectorAll(".drop-list select");
-getButton = document.querySelectorAll("form button");
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
 
 for (let i = 0; i < dropList.length; i++) {
     for (currency_code in countrycode){
@@ -10,11 +12,35 @@ for (let i = 0; i < dropList.length; i++) {
             selected = currency_code == "GHS" ? "selected" : "";
         }
         // console.log(currency_code)
-        let optionTag =  `<option value="${currency_code}" ${selected}>${currency_code}</option>`
-        dropList[i].insertAdjacentHTML("beforeend", optionTag)
+        let optionTag =  `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }    
 }
 
 getButton.addEventListener("click", e =>{
-    
+    e.preventDefault();
+    getExchangeRate();
 })
+
+window.addEventListener("load", () =>{
+    getExchangeRate();
+});
+
+function getExchangeRate(){
+    const amount = document.querySelector(".amount input");
+    const exchangeRateTxt = document.querySelector("form .exchange-rate");
+
+    let amountVal = amount.value;
+    if (amountVal == "" || amountVal == "0"){
+        amountVal = 1;
+        amount.value = "1";
+    }
+    exchangeRateTxt.innerText = "Loading rate...";
+    let url = `https://v6.exchangerate-api.com/v6/ccbc47815dd70fdb8f65ff3d/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalAmount = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalAmount} ${toCurrency.value}`;
+        console.log(totalAmount)
+    });
+}
